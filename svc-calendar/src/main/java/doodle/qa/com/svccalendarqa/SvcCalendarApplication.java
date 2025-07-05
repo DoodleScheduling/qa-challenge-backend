94a683394faf748c3776f2dc7f5c51ec1c7be214
Milos Pajic <milos.pajic@doodle.com>
2025-07-05 12:32:38 +0200

    Added multi-maven project

---
+++ svc-calendar/src/main/java/doodle/qa/com/svccalendarqa/SvcCalendarApplication.java
@@ -0,0 +1,12 @@
+package doodle.qa.com.svccalendarqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcCalendarApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SvcCalendarApplication.class, args);
+  }
+}
